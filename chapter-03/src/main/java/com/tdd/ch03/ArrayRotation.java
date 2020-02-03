6191485aa63c381c9c89ec131260a136007c107d
Senthil Rajendran <senthilar@gmail.com>
2020-02-03 07:44:41 -0800

    implemented the clockwise rotation

--- chapter-03/src/main/java/com/tdd/ch03/ArrayRotation.java
+++ chapter-03/src/main/java/com/tdd/ch03/ArrayRotation.java
@@ -11,45 +11,38 @@ public class ArrayRotation {
         int[] buffer;
         if (rotation<0) {
             //1. Buffer
-            buffer = new int[rotation * -1];
-            for (int i = 0; i < rotation*-1; i++) {
+            int cRotation = rotation * -1;
+            buffer = new int[cRotation];
+            for (int i = 0; i < cRotation; i++) {
                 buffer[i] = input[inputLength+rotation+i];
+            }
+
+            //2. Move the remaining element to tail
+            for(int i = inputLength-1; i > cRotation -1; i--) {
+                System.out.println("i="+i);
+                input[i] = input[i- cRotation];
+            }
+
+            //3.Copy the buffer to the front
+            for (int i = 0; i < cRotation; i++) {
+                input[i] = buffer[i];
             }
         }
         else {
             //1. Buffer
             buffer = new int[rotation];
             for (int i = 0; i < rotation; i++) buffer[i] = input[i];
-        }
 
-        System.out.println("Buffer: " + Arrays.toString(buffer));
-
-        if (rotation<0) {
-            //2. Move the remaining element to tail
-            for(int i = inputLength-1; i > (rotation*-1)-1; i--) {
-                System.out.println("i="+i);
-                input[i] = input[i-(rotation*-1)];
-            }
-        } else {
             //2. Move the remaining element to front
             for (int i = 0; i < inputLength - rotation; i++) {
                 input[i] = input[rotation + i];
             }
-        }
-        System.out.println("After rotation:" + Arrays.toString(input));
 
-        if (rotation < 0) {
-            //3.Copy the buffer to the front
-            for (int i = 0; i < rotation*-1; i++) {
-                input[i] = buffer[i];
-            }
-        } else {
             //3.Copy the buffer to the tail
             for (int i = 0; i < rotation; i++) {
                 input[i + inputLength - rotation] = buffer[i];
             }
         }
-        System.out.println("After copying the buffer:" + Arrays.toString(input));
 
         return input;
     }
